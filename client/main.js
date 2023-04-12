13708514e44b059b0da65b7b2d246ce7d4f6b40b
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-12 23:30:28 +0200

    basic js functionality added

--- client/main.js
+++ client/main.js
@@ -1,24 +1,80 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const form = document.querySelector("form");
+const gptContainer = document.querySelector("#gpt_container");
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+    if (element.textContent === "...") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+    <div class="wrapper ${isAi && "ai"}">
+    <div class="chat">
+    <div className="profile">
+    <img src = "${isAi ? bot : user}"/>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    <div className="message" id=${uniqueId}>${value}</div>
+    </div>
+    </div> 
+  `
+  )
+}
 
-setupCounter(document.querySelector('#counter'))
+const handleSubmit = async(e) => {
+  e.preventDefault();//avoid reloading the browser after submitting a form which is the default action 
+
+  const data = new FormData(form);
+
+  gptContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset(); //clear the input text
+
+  const uniqueId = generateUniqueId(); 
+
+  gptContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  gptContainer.scrollTop = gptContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId); 
+
+  loader(messageDiv); 
+
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup',(e)=>{
+  if(e.key="enter"){
+    handleSubmit(e);
+  }
+});
